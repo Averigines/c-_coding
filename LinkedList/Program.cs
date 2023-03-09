06c0e73339fce201874bbdb53c9301584015c242
cc211032 <cc211032@fhstp.ac.at>
2023-03-09 11:50:26 +0100

    LinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+public class Node {
+
+    // Fields
+    private int _data;
+    private Node? _next;
+
+    // Constructors
+    public Node(int d) {
+        _data = d;
+        _next = null;
+    }
+
+    // Getters and Setters
+    public int Data {
+        get { return _data; }
+        set { _data = value; }
+    }
+
+    public Node? Next {
+        get { return _next; }
+        set { _next = value; }
+    }
+
+    // Finalizers
+
+}
