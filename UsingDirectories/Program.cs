04bca85a7be822192b57366fd4b88a7113daa816
cc211032 <cc211032@fhstp.ac.at>
2023-04-14 11:12:29 +0200

    UsingDirectoriesFinished

---
+++ UsingDirectories/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+WorkWithDirectories();
+
+static void WorkWithDirectories() {
+    // define a directory path for a new folder
+    string newFolder = Combine(GetFolderPath(SpecialFolder.Desktop), "MyNewFolder");
+    Console.WriteLine("Working with: {newFolder}");
+    Console.WriteLine($"Does it exist? {Directory.Exists(newFolder)}");
+
+    //Create a directory
+
+    Console.WriteLine("Creating the folder...");
+    CreateDirectory(newFolder);
+    Console.WriteLine($"Does it exist? {Directory.Exists(newFolder)}");
+    Console.WriteLine("Confirm the directory exist, and then press ENTER:");
+    Console.ReadLine();
+
+    //Delete directory
+
+    Console.WriteLine("Deleteing the folder...");
+    Delete(newFolder, recursive: true);
+    Console.WriteLine($"Does it exist? {Directory.Exists(newFolder)}");
+}
