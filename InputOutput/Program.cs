676d39003eeef3b299113e9cde52799eb9f5a2fa
cc211032 <cc211032@fhstp.ac.at>
2023-04-14 10:36:08 +0200

    finish input

--- InputOutput/Program.cs
+++ InputOutput/Program.cs
@@ -20,3 +20,21 @@ if (!string.IsNullOrEmpty(newCulture)) {
     CultureInfo.CurrentCulture = ci;
     CultureInfo.CurrentUICulture = ci;
 }
+
+Console.WriteLine();
+Console.Write("Enter your name: ");
+string? name = Console.ReadLine();
+
+Console.WriteLine("Enter your date of birth: ");
+string? dob = Console.ReadLine();
+
+Console.WriteLine("Enter your salary: ");
+string? salary = Console.ReadLine();
+
+if (name != null && dob != null && salary != null) {
+    DateTime date = DateTime.Parse(dob);
+    int minutes = (int)DateTime.Today.Subtract(date).TotalMinutes;
+    decimal earns = decimal.Parse(salary);
+
+    Console.WriteLine("{0} was born on a {1:dddd}, is {2:N0} minutes old, and earns {3:C}", name, date, minutes, earns);
+}
