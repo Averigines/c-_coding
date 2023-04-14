d989f6d5afa1538791e7ef3c173bd31a67c0657a
cc211032 <cc211032@fhstp.ac.at>
2023-04-14 11:18:43 +0200

    WorkWithText

---
+++ WorkWithText/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+class Program {
+
+    static string[] callsigns = new string[] {
+        "ABC", "TEST", "BCC", "BMT"
+    };
+
+    static void Main(string[] args) {
+        WorkWithText();
+    }
+
+    static void WorkWithText() {
+        string textFile = Combine(CurrentDirectory, "streams.txt");
+
+        //create a text file and return a helper writer
+        StreamWriter text = File.CreateText(textFile);
+
+        foreach (string item in callsigns) {
+            text.WriteLine(item);
+        }
+        text.Close();
+
+        //release the resource
+    }
+}
